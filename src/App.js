e2f939bd2c5f64fa29bc48f1e7b48a1c3ba2e51c
extremistLab <adarsh.chb@gmail.com>
2023-12-30 21:39:25 +0530

    FirstCommitForAPP

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's go for the last ride!</h2>
+      <p>This is me!!</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>DATE</div>
+            <div>
+            <h2>Food</h2>
+            <div>Rs 10</div>
+            <h2>Petrol</h2>
+            <div>Rs 100</div>
+            <h2>Movies</h2>
+            <div>Rs 200</div>
+            </div>
+        </div>
+        
+    );
+}
+
+export default ExpenseItem;
