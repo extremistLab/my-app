5e40ef9a86f9d97df44ea07fccc974012d79ff17
extremistLab <adarsh.chb@gmail.com>
2024-01-04 22:01:47 +0530

    components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,13 @@
-function ExpenseItem() {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+
     return (
-        <div>
-            <div>DATE</div>
-            <div>
-            <h2>Food</h2>
-            <div>Rs 10</div>
-            <h2>Petrol</h2>
-            <div>Rs 100</div>
-            <h2>Movies</h2>
-            <div>Rs 200</div>
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">Rs {props.amount}</div>
             </div>
         </div>
         
